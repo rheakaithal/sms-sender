48a7654dbae565253f0993a9a8eee0cb28e1d919
rhea-wgai <rhea@whitegloveai.com>
2025-05-31 23:42:12 -0500

    adding info to site (this is backend only)

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ app.use('/api', smsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
+  app.get('/', (req, res) => {
+  res.send('SMS Sender API is running. Use POST /api/send to send messages.');
+});
+
 });
